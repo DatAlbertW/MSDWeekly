24c135394e94612426277832efb37eaa02e2f143
DatAlbertW <144963224+DatAlbertW@users.noreply.github.com>
2024-04-27 12:22:29 -0600

    Update app.py

--- week.7 (datacollectionservice)/app.py
+++ week.7 (datacollectionservice)/app.py
@@ -1,55 +1,29 @@
-import datastorage
-
 import json
-from flask import request, Flask
+from flask import Flask, request
+import datastorage
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def index():
-    return json.dumps({'name' : 'David',
-    'mail' : 'david.herzig@roche.com'})
+@app.route('/data', methods=['POST'])
+def add_data():
+    """Add data to storage."""
+    data = request.get_json()
+    if 'id' in data:
+        ds = datastorage.DataStorage()
+        ds.store_data(data['id'], data)
+        return "Data added", 201
+    else:
+        return "Missing ID", 400
 
-@app.route('/experiment', methods=['POST', 'GET'])
-def create_experiment():
-  ds = datastorage.DataStorage()
-  if request.method == 'POST':
-    form_data = request.data
-    data = json.loads(form_data)
-    id = ds.create_experiment(data['name'])
-    return json.dumps({'result' : id})
-  elif request.method == 'GET':
-    return ds.get_experiments()
-  
-@app.route('/patient', methods=['POST', 'GET'])
-def create_patient():
-  ds = datastorage.DataStorage()
-  if request.method == 'POST':
-    name = request.args.get('name')
+@app.route('/data/<int:data_id>', methods=['GET'])
+def get_data(data_id):
+    """Retrieve data by ID."""
     ds = datastorage.DataStorage()
-    id = ds.create_patient(name)
-    return json.dumps({'result' : id})
-  elif request.method == 'GET':
-    return ds.get_patients()
-  
-@app.route('/store', methods=['POST'])
-def store_data():
-  pass
-  
-@app.route('/upload', methods=['POST'])
-def upload_data(data):
-  pass
-  
+    result = ds.retrieve_data(data_id)
+    if result:
+        return json.dumps(result), 200
+    else:
+        return "Data not found", 404
 
 if __name__ == '__main__':
-  print('Hello World')
-  
-  ds = datastorage.DataStorage()
-  print(ds.create_experiment('Experiment'))
-  print(ds.get_experiments())
-  
-  ds2 = datastorage.DataStorage()
-  print(ds2.create_experiment('Experiment2'))
-  print(ds2.get_experiments())
-  
-
+    app.run(debug=True)
